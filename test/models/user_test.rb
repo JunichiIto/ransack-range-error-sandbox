dae3470bc669fec600bac6ddf4a121301ecd4381
Junichi Ito <jit@sonicgarden.jp>
2022-06-09 13:56:44 +0900

    Add test for ActiveModel::RangeError

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "ActiveModel::RangeError" do
+    user = users(:one)
+    assert_equal [user], User.where(age: 10)
+    assert_equal [user], User.ransack(age_eq: 10).result
+
+    assert_equal [], User.where(age: 123456789012345678901)
+    assert_equal [], User.ransack(age_eq: 123456789012345678901).result
+  end
 end
