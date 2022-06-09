84f0c109f6c0d1bc827e7832a5a11c81045324d0
Junichi Ito <jit@sonicgarden.jp>
2022-06-09 17:08:18 +0900

    Add comment

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,10 +3,10 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   test "ActiveModel::RangeError" do
     user = users(:one)
-    assert_equal [user], User.where(age: 10)
-    assert_equal [user], User.ransack(age_eq: 10).result
+    assert_equal [user], User.where(age: 10) # pass
+    assert_equal [user], User.ransack(age_eq: 10).result # pass
 
-    assert_equal [], User.where(age: 123456789012345678901)
+    assert_equal [], User.where(age: 123456789012345678901) # pass
     assert_equal [], User.ransack(age_eq: 123456789012345678901).result
     #=> ActiveModel::RangeError: 123456789012345678901 is out of range for ActiveRecord::ConnectionAdapters::SQLite3Adapter::SQLite3Integer with limit 8 bytes
   end
